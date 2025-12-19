3e159c6505d15a57ec3d5191378a306f675142ef
Satyam Singh <satyamsingh0159@gmail.com>
2025-12-19 15:52:46 +0530

    Intial Commit

---
+++ FirstApp/Day 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Console.Beep();
+        
+        //  ***to calculate Area***
+        //AOC.Area();
+
+        //  ***to convert given length in CM from feet***
+        //Conversion.FtoCM();
+
+        //  ***to convert given Seconds in Minutes***
+        //Con.StoM();
+
+        //  *** Conditional Statement***
+        //Conditions.Condition();
+
+        //  *** Odd Even ***
+        //OE.OddEven();
+
+        // *** eligibility for DL using If else ***
+        //DL.Dl();
+
+        // *** largest number among 3 numbers ***
+        //Large.Largest();
+
+        //*** sum of digits using while loop ***
+        //Sum.OfDigits();
+
+        //*** reverse a digit ***
+        //Rev.erse();
+        
+        
+        //          **** day 2     ****
+        
+        //  *** Do While loop ***
+        //Do.Whle();
+
+        //  ***Multiplication table of 21 to 30 ***
+        //Multiplication.Example2();
+
+        //  ***Small Game using For Loop***
+        //Game.Example1();
+
+        //  ***   Loan Eligibility,Tax Calculation,Transactions     ***
+        //Finance.Manage();
+
+
+        Bank.Credit_Debit();
+    }
+
+}
